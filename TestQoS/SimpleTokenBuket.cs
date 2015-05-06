2b6655ce627568b62146ad9fd51f32d853ca9ece
Lemiort <lemiort@gmail.com>
2015-05-06 23:37:20 +0300

    Traffic generator tokens parameter
    Added traffic generator parameter

--- TestQoS/SimpleTokenBuket.cs
+++ TestQoS/SimpleTokenBuket.cs
@@ -8,11 +8,86 @@ namespace TestQoS
 {
     class SimpleTokenBuket : TokenBuket
     {
+        /// <summary>
+        /// число токенов в милисекунду
+        /// </summary>
+        public float TokensPerMs
+        {
+            get;
+            set;
+        }
 
-        public SimpleTokenBuket()
+        /// <summary>
+        /// время обработки предыдущего пакета
+        /// </summary>
+        private long prevPacketTime;
+
+        /// <summary>
+        /// число токенов
+        /// </summary>
+        private long tokensCount;
+
+        /// <summary>
+        /// обработчик события обработки пакета
+        /// </summary>
+        /// <param name="packet">пакет или null</param>
+        public delegate void PacketProcessHandler(Packet packet);
+
+        /// <summary>
+        /// отбрасывание или пропуск пакета
+        /// </summary>
+        /// <param name="packet">пакет на вход</param>
+        /// <returns>если пакет проходит, возвращает пакета, иначе null</returns>
+        public Packet ProcessPacket(Packet packet)
         {
 
+            //проверка на ненулевой пакет
+            if (packet != null)
+            {
+                //считаем изменение времени
+                //TODO: сделать поправку на то, что Tick != милисекундам!!!
+                long dt = DateTime.Now.Ticks - prevPacketTime;
+
+                //время в милисекундах
+                TimeSpan time = new TimeSpan(dt);
+
+                //добавляем токены
+                tokensCount += (long)(time.Milliseconds * TokensPerMs);
+
+                //проверяем пакет
+                if (packet.Size < tokensCount)
+                {
+                    tokensCount -= packet.Size;
+
+                    //пропускаем пакет
+                    onPacketPass(packet);
+                    return packet;
+                }
+                else
+                {
+                    //не пропускаем пакет
+                    onPacketNotPass(packet);
+                    return null;
+                }
+            }
+            else
+            {
+                return null;
+            }
         }
+
+        /// <summary>
+        /// конструктор
+        /// TODO: написать конструктор получше
+        /// </summary>
+        public SimpleTokenBuket()
+        {
+            prevPacketTime = DateTime.Now.Ticks;
+            tokensCount = 0;
+        }
+
+        public event PacketProcessHandler onPacketPass;
+        public event PacketProcessHandler onPacketNotPass;
 
     }
 }
