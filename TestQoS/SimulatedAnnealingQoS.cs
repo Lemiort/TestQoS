967ad9ddde67136209899d1302def83710785541
FreeWind94 <shukin-nikita@mail.ru>
2015-05-25 02:26:18 +0300

    AverageStrategyQos. Begining
    ну что, начало положено

---
+++ TestQoS/SimulatedAnnealingQoS.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestQoS
+{
+    /// <summary>
+    /// это пиздец, товарищи
+    /// </summary>
+    class SimulatedAnnealingQoS : SimpleTBQoS
+    {
+    }
+}
